b9330bb84d7e711c624b93eeae41d2b22673350a
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-05-10 01:57:19 +0530

    ADD: state and handleChange for textarea

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [text, setText] = useState('');
+  const handleChange = e => setText(e.target.value);
+
   return (
     <>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time reminaing: __</h4>
       <button>Start</button>
       <h1>Word count: ???</h1>

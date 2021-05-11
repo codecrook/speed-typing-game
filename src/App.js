4c50838b295ce26cd888e2d0516a16f459bdbd34
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-05-12 00:34:39 +0530

    ADD: function to calculate word count

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React, { useState } from 'react';
 function App() {
   const [text, setText] = useState('');
   const handleChange = e => setText(e.target.value);
+  const calculateWordCount = text => text.trim().split(' ').filter(Boolean).length;
 
   return (
     <>
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time reminaing: __</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
+      <h4>Time Remaining: __</h4>
+      <button
+        onClick={() => console.log(calculateWordCount(text))}
+      >
+        Start
+      </button>
+      <h1>Word count: __</h1>
     </>
   );
 }

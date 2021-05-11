648fea9b0b2ebbd4d267f576956c240aa116a34f
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-05-12 01:33:58 +0530

    ADD: functionality to count wors typed within time frame

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const STARTING_TIME = 10;
+
   const [text, setText] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   const handleChange = e => setText(e.target.value);
   const calculateWordCount = text => text.trim().split(' ').filter(Boolean).length;
 
+  const startGame = () => {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+  };
+
+  const endGame = () => {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(text));
+  }
+
   useEffect(() => {
     if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => setTimeRemaining(t => t - 1), 1000);
+      setTimeout(() => setTimeRemaining(t => t - 1), 1000)
     } else {
-      setIsTimeRunning(false);
+      endGame();
     }
   }, [timeRemaining, isTimeRunning]);
 
   return (
     <>
       <h1>How fast do you type?</h1>
-      <textarea value={text} onChange={handleChange} />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        disabled={!isTimeRunning}
+      />
       <h4>Time Remaining: {timeRemaining}</h4>
       <button
-        onClick={() => setIsTimeRunning(true)}
+        onClick={startGame}
+        disabled={isTimeRunning}
       >
         Start
       </button>
-      <h1>Word count: __</h1>
+      <h1>Word count: {wordCount}</h1>
     </>
   );
 }

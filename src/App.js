7c7fcbe43a369aeff9e31c8ed3e7c8a4cd210cb5
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-05-12 00:57:35 +0530

    ADD: effect to handle time remaining countdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState('');
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
   const handleChange = e => setText(e.target.value);
   const calculateWordCount = text => text.trim().split(' ').filter(Boolean).length;
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => setTimeRemaining(t => t - 1), 1000);
+    } else {
+      setIsTimeRunning(false);
+    }
+  }, [timeRemaining, isTimeRunning]);
 
   return (
     <>
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time Remaining: __</h4>
+      <h4>Time Remaining: {timeRemaining}</h4>
       <button
-        onClick={() => console.log(calculateWordCount(text))}
+        onClick={() => setIsTimeRunning(true)}
       >
         Start
       </button>
